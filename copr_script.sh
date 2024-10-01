ddf3b5820ed040cd6bcbdd5dfadecc1e5144a63a
Root <175176948+RKNF404@users.noreply.github.com>
2024-10-01 16:01:02 -0400

    Add source compression instead of using prebuilt tools

--- copr_script.sh
+++ copr_script.sh
@@ -2,15 +2,9 @@
 
 # Preset variables
 readonly SPEC_FILE="hardened-chromium-subresource-filter.spec"
-readonly CONVERTER_FILE="ruleset_converter"
-readonly GIT_URL="https://raw.githubusercontent.com/secureblue/hardened-chromium-subresource-filter/refs/heads/master/"
-
-# Checksums use SHA384
-readonly SPEC_CHECKSUM="3a75b3b1605e11b8c084775ab92efddcb3e106c441d176ff62c3c4b546b04181ad04a60b1d251b84a101dd9830fc7d9b"
-readonly CONVERTER_CHECKSUM="28e225e884b2e657962ddb1b8900c41782e21a5c6208c30f75617d83bdaabc7027f0eaf3be873aa99d5a794d62c72d30"
-
-LISTS="easylist.txt,easyprivacy.txt"
-LIST_SOURCES="https://easylist.to/easylist/easylist.txt https://easylist.to/easylist/easyprivacy.txt"
+readonly SPEC_SHA384_CHECKSUM="3a75b3b1605e11b8c084775ab92efddcb3e106c441d176ff62c3c4b546b04181ad04a60b1d251b84a101dd9830fc7d9b"
+readonly GIT_URL="https://raw.githubusercontent.com/secureblue/hardened-chromium-subresource-filter/refs/heads/"
+readonly LIST_SOURCES="https://easylist.to/easylist/easylist.txt https://easylist.to/easylist/easyprivacy.txt"
 
 # Get a file from whatever source and, given a checksum, validate it
 download_and_verify() {
@@ -35,18 +29,21 @@ download_and_verify() {
 	done
 }
 
-download_and_verify $SPEC_FILE $GIT_URL/$SPEC_FILE $SPEC_CHECKSUM
-download_and_verify $CONVERTER_FILE.tar.xz $GIT_URL/$CONVERTER_FILE.tar.xz $CONVERTER_CHECKSUM
+download_and_verify $SPEC_FILE $GIT_URL/$SPEC_FILE $SPEC_SHA384_CHECKSUM
+
+# Get chromium's source and depot tools
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH="$(pwd)/depot_tools:$PATH"
+mkdir ./chromium && cd ./chromium
+fetch --nohooks --no-history chromium
+cd src
+gclient runhooks
+gn gen out/Release
+cd ../../
+
+# Compress for later use
+zip -r chromium.zip chromium
+zip -r depot_tools.zip depot_tools
 
 # Get the filters that will be added
-for source in $LIST_SOURCES; do
-	wget $source
-done
-
-# Run the converter file to generate the filterlist
-tar xvf $CONVERTER_FILE.tar.xz
-./$CONVERTER_FILE/$CONVERTER_FILE --input_format=filter-list --output_format=unindexed-ruleset --input_files=$LISTS --output_file=hardened-chromium-blocklist
-
-# Cleanup (only needed for a local, non-copr, run)
-rm easylist.txt easyprivacy.txt
-rm -rf ruleset_converter/
+wget $LIST_SOURCES
