eb0c46577861688c572883caa62ec4be021fd7ad
Root <175176948+RKNF404@users.noreply.github.com>
2024-10-08 23:45:28 -0400

    chore: update copr script to not pull repo

--- copr_script.sh
+++ copr_script.sh
@@ -4,17 +4,14 @@
 readonly VERSION="129.0.6668.100" # update this every now-and-again
 readonly LIST_SOURCES="https://easylist.to/easylist/easylist.txt https://easylist.to/easylist/easyprivacy.txt"
 readonly NAME="hardened-chromium-subresource-filter"
-readonly GIT_URL="https://github.com/secureblue/$NAME.git"
 
-# Clone the repo with the spec file and chrowmium source downloader
-git clone $GIT_URL
+# Copy the spec file and chromium source downloader from the cloned repo
 cp $NAME/$NAME.spec ./
 cp $NAME/install_filter.sh ./
 cp $NAME/chromium-latest.py ./
 python3 ./chromium-latest.py --version $VERSION --stable --cleansources
 rm chromium-$VERSION.tar.xz
 rm -rf ./chromium-$VERSION
-rm -rf ./$NAME
 
 # Get the filters that will be added
 wget $LIST_SOURCES
