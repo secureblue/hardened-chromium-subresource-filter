40d89e9145443156ceec13e459ab28e0877f705a
Root <175176948+RKNF404@users.noreply.github.com>
2024-10-08 22:12:00 -0400

    chore: bump copr script version

--- copr_script.sh
+++ copr_script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -x
 
 # Preset variables
-readonly VERSION="129.0.6668.89" # update this every now-and-again
+readonly VERSION="129.0.6668.100" # update this every now-and-again
 readonly LIST_SOURCES="https://easylist.to/easylist/easylist.txt https://easylist.to/easylist/easyprivacy.txt"
 readonly NAME="hardened-chromium-subresource-filter"
 readonly GIT_URL="https://github.com/secureblue/$NAME.git"
