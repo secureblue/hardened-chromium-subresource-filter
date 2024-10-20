3454c1eefd5cc31c9acb13fdc5d468a850462bff
qoijjj <129108030+qoijjj@users.noreply.github.com>
2024-10-19 20:55:35 -0700

    fix: use array for list_sources in copr_script

--- copr_script.sh
+++ copr_script.sh
@@ -2,31 +2,33 @@
 
 # Preset variables
 readonly VERSION="130.0.6723.58" # update this every now-and-again
-readonly LIST_SOURCES="https://easylist.to/easylist/easylist.txt \ 
-https://easylist.to/easylist/easyprivacy.txt \ 
-https://secure.fanboy.co.nz/fanboy-annoyance.txt \ 
-https://raw.githubusercontent.com/heradhis/indonesianadblockrules/master/subscriptions/abpindo.txt \ 
-https://abpvn.com/filter/abpvn-IPl6HE.txt \ 
-https://stanev.org/abp/adblock_bg.txt \ 
-https://raw.githubusercontent.com/DandelionSprout/adfilt/master/NorwegianExperimentalList%20alternate%20versions/NordicFiltersABP-Inclusion.txt \ 
-https://easylist-downloads.adblockplus.org/easylistchina.txt \ 
-https://raw.githubusercontent.com/tomasko126/easylistczechandslovak/master/filters.txt \ 
-https://easylist-downloads.adblockplus.org/easylistdutch.txt \ 
-https://easylist.to/easylistgermany/easylistgermany.txt \ 
-https://raw.githubusercontent.com/easylist/EasyListHebrew/master/EasyListHebrew.txt \ 
-https://easylist-downloads.adblockplus.org/easylistitaly.txt \ 
-https://raw.githubusercontent.com/EasyList-Lithuania/easylist_lithuania/master/easylistlithuania.txt \ 
-https://easylist-downloads.adblockplus.org/easylistpolish.txt \ 
-https://easylist-downloads.adblockplus.org/easylistportuguese.txt \ 
-https://easylist-downloads.adblockplus.org/easylistspanish.txt \ 
-https://easylist-downloads.adblockplus.org/indianlist.txt \ 
-https://easylist-downloads.adblockplus.org/koreanlist.txt \ 
-https://raw.githubusercontent.com/Latvian-List/adblock-latvian/master/lists/latvian-list.txt \ 
-https://easylist-downloads.adblockplus.org/liste_ar.txt \ 
-https://easylist-downloads.adblockplus.org/liste_fr.txt \ 
-https://zoso.ro/pages/rolist.txt \ 
-https://easylist-downloads.adblockplus.org/ruadlist.txt \ 
-https://easylist-downloads.adblockplus.org/antiadblockfilters.txt"
+readonly LIST_SOURCES=(
+    "https://easylist.to/easylist/easylist.txt"
+    "https://easylist.to/easylist/easyprivacy.txt"
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt"
+    "https://raw.githubusercontent.com/heradhis/indonesianadblockrules/master/subscriptions/abpindo.txt"
+    "https://abpvn.com/filter/abpvn-IPl6HE.txt"
+    "https://stanev.org/abp/adblock_bg.txt"
+    "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/NorwegianExperimentalList%20alternate%20versions/NordicFiltersABP-Inclusion.txt"
+    "https://easylist-downloads.adblockplus.org/easylistchina.txt"
+    "https://raw.githubusercontent.com/tomasko126/easylistczechandslovak/master/filters.txt"
+    "https://easylist-downloads.adblockplus.org/easylistdutch.txt"
+    "https://easylist.to/easylistgermany/easylistgermany.txt"
+    "https://raw.githubusercontent.com/easylist/EasyListHebrew/master/EasyListHebrew.txt"
+    "https://easylist-downloads.adblockplus.org/easylistitaly.txt"
+    "https://raw.githubusercontent.com/EasyList-Lithuania/easylist_lithuania/master/easylistlithuania.txt"
+    "https://easylist-downloads.adblockplus.org/easylistpolish.txt"
+    "https://easylist-downloads.adblockplus.org/easylistportuguese.txt"
+    "https://easylist-downloads.adblockplus.org/easylistspanish.txt"
+    "https://easylist-downloads.adblockplus.org/indianlist.txt"
+    "https://easylist-downloads.adblockplus.org/koreanlist.txt"
+    "https://raw.githubusercontent.com/Latvian-List/adblock-latvian/master/lists/latvian-list.txt"
+    "https://easylist-downloads.adblockplus.org/liste_ar.txt"
+    "https://easylist-downloads.adblockplus.org/liste_fr.txt"
+    "https://zoso.ro/pages/rolist.txt"
+    "https://easylist-downloads.adblockplus.org/ruadlist.txt"
+    "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt"
+)
 readonly NAME="hardened-chromium-subresource-filter"
 
 # Clone the repo with the spec file and chrowmium source downloader
@@ -39,4 +41,6 @@ rm -rf ./chromium-$VERSION
 rm -rf ./$NAME
 
 # Get the filters that will be added
-wget $LIST_SOURCES
+for url in "${LIST_SOURCES[@]}"; do
+    wget "$url"
+done
