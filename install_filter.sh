3b02158ee0d21429b617494770ce6b78f8760072
Root <175176948+RKNF404@users.noreply.github.com>
2025-06-02 13:37:11 -0400

    chore: add more clarity to output statements

--- install_filter.sh
+++ install_filter.sh
@@ -6,21 +6,22 @@ readonly OLD_DIR="$HOME/.config/trivalent"
 readonly FILTER_VER="$(cat $INSTALL_DIR/trivalent-blocklist-version.txt)"
 readonly CURRENT_VER="$(ls $OLD_DIR/Subresource\ Filter/Unindexed\ Rules)"
 
-echo "Checking version" # Debug statement
-echo "  Installed version: $CURRENT_VER" # Debug statement
-echo "  Package version: $FILTER_VER" # Debug statement
+echo "Checking Subresource Filter version..."
+echo "  Installed version: $CURRENT_VER"
+echo "  Package version: $FILTER_VER"
 if [ "$FILTER_VER" == "$CURRENT_VER" ]; then
-  echo "No need to update, versions match" # Debug statement
+  echo "No need to update, versions match"
   exit 0
 fi
+echo "Version mismatch, updating filter..."
 
 readonly NEW_DIR="$OLD_DIR/Subresource Filter/Unindexed Rules/$FILTER_VER"
 
-echo "Removing '$OLD_DIR/Subresource Filter'" # Debug statement
+echo "Removing '$OLD_DIR/Subresource Filter'"
 rm -r "$OLD_DIR/Subresource Filter"
-echo "Creating '$NEW_DIR'" # Debug statement
+echo "Creating '$NEW_DIR'"
 mkdir -p "$NEW_DIR"
-echo "Adding filter list from '$INSTALL_DIR'" # Debug statement
+echo "Adding filter list from '$INSTALL_DIR'"
 cp "$INSTALL_DIR/trivalent-blocklist" "$NEW_DIR/Filtering Rules"
 echo "Creating 'manifest.json'"
 cat << EOF > "$NEW_DIR/manifest.json"
@@ -31,3 +32,4 @@ cat << EOF > "$NEW_DIR/manifest.json"
   "version": "$FILTER_VER"
 }
 EOF
+echo "Subresource Filter successfully updated"
