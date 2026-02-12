121a229ad9b1090126b29ef824fbf9ad90756d43
Rootkit404 <175176948+RKNF404@users.noreply.github.com>
2026-02-12 18:47:40 -0500

    Update copr_script.sh

--- copr_script.sh
+++ copr_script.sh
@@ -56,7 +56,7 @@ cp /usr/src/chromium/chromium-version.txt ./
 rm -rf "./$NAME"
 
 # Get the filters that will be added
-counter=1
+declare -i counter=1
 for url in "${LIST_SOURCES[@]}"; do
     wget "$url" -O "filter-$counter.txt"
     counter=$((counter+1))
